71fc403ccc9f041060c753fa21f86333a9c16874
Éloi Strée <eloistree@gmail.com>
2023-01-06 00:37:55 +0100

    Version of the project where we use Visual studio instead of unity.

---
+++ ZorroFiltering/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace ZorroFiltering
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello 7orro !");
+
+            //Console.WriteLine("Do you allow uppercase (y)?");
+            //string upperCaseAllowString = Console.ReadLine();
+            //bool isUpperCaseAllow = upperCaseAllowString == "y";
+
+
+
+            Console.WriteLine("Can you give me the path of the file to filter:");
+            string filePath = Console.ReadLine();
+            Console.WriteLine("Can you give me the export file name :");
+            string fileExportName = Console.ReadLine();
+            if (fileExportName.IndexOf(".") < 0)
+                fileExportName += ".txt";
+
+            string fileExport = Path.GetDirectoryName(filePath) + "/" + fileExportName;
+
+            TestZorro zorro = new TestZorro();
+            zorro.m_fileImportPath = filePath;
+            zorro.m_fileExportPath = fileExport;
+            zorro.ProcessWords();
+        }
+
+
+}
+}
---
+++ ZorroFiltering/TestZorro.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ZorroFiltering
+{
+        public class TestZorro 
+    {
+
+        public string m_fileImportPath="";
+        public string m_fileExportPath="" ;
+
+        public string m_listOfWordsAsText = "";
+
+        List<string> m_wordsBeforeFilter = new List<string>();
+        List<string> m_wordsAfterFilter = new List<string>();
+        public string m_resultText="";
+
+        public void ProcessWords()
+        {
+            m_listOfWordsAsText = File.ReadAllText(m_fileImportPath);
+
+            m_wordsBeforeFilter.Clear();
+            m_wordsAfterFilter.Clear();
+
+            m_wordsBeforeFilter = m_listOfWordsAsText.Split("\n").ToList();
+            foreach (var word in m_wordsBeforeFilter)
+            {
+                if (DicoSolution_HasDoubleCharInText(word)) { }
+                else m_wordsAfterFilter.Add(word);
+
+            }
+            m_resultText = string.Join("\n", m_wordsAfterFilter);
+
+            File.WriteAllText(m_fileExportPath, m_resultText);
+        }
+
+
+        public static bool DicoSolution_HasDoubleCharInText(string word)
+        {
+            // eloirroio
+            // elroiro
+            word = word.Trim();
+            Dictionary<char, int> dico = new Dictionary<char, int>();
+            foreach (char c in word.ToCharArray())
+            {
+                if (!dico.ContainsKey(c))
+                    dico.Add(c, 1);
+                else dico[c] += 1;
+
+                if (dico[c] > 1)
+                    return true;
+            }
+            return false;
+        }
+    
+
+
+}
+}
